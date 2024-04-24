7cef32c4013338c0ffd03e71a2e89289200e034d
Shadycrisp <142788636+Shadycrisp@users.noreply.github.com>
2024-04-24 21:16:11 +0300

    Made filtering script work with new data format

--- PythonDataAnalysis/filter.py
+++ PythonDataAnalysis/filter.py
@@ -1,61 +1,70 @@
 from coToDis import calculate_distance
-#Variables
+import matplotlib.pyplot as plt
+
 temps = []
 pressures = []
 altitude = []
 lattitude = []
 longditude = []
 locations = []
-filepath = "data.txt.txt"
-
+degrees = []
+distances = []
+MagY = []
+packetNum = 6
+filepath = "data2.txt"
+fig, ax = plt.subplots()
 
 datafile = open(filepath, "r")
 
 lines = datafile.readlines()
+
 for line in lines:
-    #Temp
-    if "temperature" in line.lower():
-        line = line.replace("Temperature:", "")
-        line = line.replace("C", "")
-        
-        temps.append(line)
-    #Pressure
-    if "pressure" in line.lower():
-        line = line.replace("Pressure:","")
-        line = line.replace("hPa", "")
-        pressures.append(line)
-    #altitude
-    if "altitude" in line.lower():
-        line = line.replace("Altitude:","")
-        line = line.replace("m","")
-        altitude.append(line)
-    #locations
-    if "location" in line.lower():
-        line = line.replace("Location:","")
-        locSplit = line.split(",")
-        lat = float(locSplit[0])
-        lon = float(locSplit[1])
-        locations.append((lat, lon))
-        
-print("Temps")
-for i in temps:
-    print(i)
-print("Pressures")
-for i in pressures:
-    print(i)
-print("altitudes")
-for i in altitude:
-    print(i)
+    if "bmp" in line.lower():
+        lineAr = line.replace(",", " ").split()
+        temps.append(lineAr[1])
+        pressures.append(lineAr[2])
+        altitude.append(lineAr[3])
+    if "gps" in line.lower():
+        lineAr = line.replace(",", " ").split()
+        lattitude.append(lineAr[1])
+        longditude.append(line[2])
+        locations.append((lineAr[1],lineAr[2]))
+        degrees.append(lineAr[3])
+    if "mag" in line.lower():
+        lineAr = line.replace(",", " ").split()
+        MagY.append(lineAr[1])
 
-totaldistance = 0
-for i in range(len(locations)-1):
-    
-    lat1, lon1 = locations[i]
-    lat2, lon2 = locations[i+1]
-    print(lat1, lon1, lat2, lon2)
-    distance = calculate_distance(lat1, lon1, lat2, lon2)
-    totaldistance += distance
-    
+time = packetNum/2;
+timestamp = []
+for i in range (0, len(temps), 1):
+    timestamp.append(i);
 
-print(totaldistance)
+def plot(xLabel, yLabel, title, xpoints, ypoints, path):
+    
+    ax.set_xlabel(xLabel)
+    ax.set_ylabel(yLabel)
+    ax.set_title(title)
+
+    plt.plot(xpoints, ypoints)
+    plt.savefig(path)
+    plt.show()
+
+
+plot("Time(s)", "Pressure(hPa)", "Pressure over Time", timestamp, pressures, "presstime.png")
+
+plot("Time(s)", "Temperature(C)", "Temperature over Time", timestamp, temps, "temptime.png")
+
+plot("Time(s)", "Altitude(m)", "Altitude over Time", timestamp, altitude, "alttime.png")
+
+
+
+
+
+
+
+
+
+
+
+
         
